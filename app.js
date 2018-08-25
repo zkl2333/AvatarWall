3512b15089c29eee14d77d3cd77d9733e5864ad8
zkl2333 <i@zkl2333.com>
2018-08-25 22:09:49 +0800

    收编动画方法

--- app.js
+++ app.js
@@ -1,101 +1,131 @@
-var wsUrl = 'ws://123.207.167.163:9010/ajaxchattest'
-// var wsUrl = 'ws://192.168.31.92:8888/chat/zkl'
-
 var ws //websocket实例
 
-var lockReconnect = false //避免重复连接
 
-/* 
- * 建立一个直角坐标系
- * createArr(几行, 几列)
- * 返回一个顺序的坐标数组
- */
-function createArr(x, y) {
-	var arr = new Array()
-	for (var i = 0; i < x; i++) {
-		for (var j = 0; j < y; j++) {
-			arr.push({
-				x: i,
-				y: j
-			})
-		}
-	}
-	return arr
+var wsConf = {
+	// wsUrl: 'ws://192.168.31.92:8888/chat/zkl',
+	wsUrl: 'ws://123.207.167.163:9010/ajaxchattest',
+	lockReconnect: false, //避免重复连接
+	HeartBeatStr: 'HeartBeat',
 }
 
-/* 
- * 打乱数组
- * 返回乱序的新数组
- */
-function roa(arr) //arr为可能出现的元素集合
-{
-	var temp = new Array() //temp存放生成的随机数组
-	var count = arr.length
-	for (var i = 0; i < count; i++) {
-		var num = Math.floor(Math.random() * arr.length) //生成随机数num
-		temp.push(arr[num]) //获取arr[num]并放入temp
-		arr.splice(num, 1)
-	}
-	return temp
-}
-
-/* 
- * 创建动画完成事件
- * 通过创建一个div元素，判断他的style属性来确定
- * 返回带有当前浏览器前缀的“动画完成事件”
- */
-function animationEnd() {
-	var el = document.createElement('div')
-
-	// 各种浏览器的动画完成事件
-	var animations = {
-		animation: 'animationend',
-		OAnimation: 'oAnimationEnd',
-		MozAnimation: 'mozAnimationEnd',
-		WebkitAnimation: 'webkitAnimationEnd',
-	}
-
-	// 枚举事件是否定义
-	for (var t in animations) {
-		if (el.style[t] !== undefined) {
-			// 返回事件
-			return animations[t]
-		}
-	}
-}
-
-/* 
- * 向页面插入头像
- * addImg(src, x, y, ani)
- * 返回值为创建的元素
- */
-var imgId
-
-function addImg(src, x, y) {
-	try {
-		if (src != undefined && x != undefined && y != undefined) {
-			var imgEl = $('<img>')
-			imgEl.attr({
-				src: src
-			}).css({
-				left: x * 3 + 'vw',
-				top: y * 3 + 'vw',
-				position: 'absolute',
-				width: '3vw'
-			}).addClass('animated imgAnimation').appendTo('.tx').one(
-				animationEnd(),
-				function () {
-					$(this).addClass('animated fast flash')
+var wall = {
+	/* 
+	 * 建立一个直角坐标系
+	 * createArr(几行, 几列)
+	 * 返回一个顺序的坐标数组
+	 */
+	createArr: function createArr(x, y) {
+		var arr = new Array()
+		for (var i = 0; i < x; i++) {
+			for (var j = 0; j < y; j++) {
+				arr.push({
+					x: i,
+					y: j
 				})
-			return imgEl
-		} else {
-			throw new Error('缺少参数')
+			}
 		}
-
-	} catch (error) {
-		console.log('创建图片失败', error, src, x, y)
+		return arr
+	},
+	/* 
+	 * 打乱数组
+	 * 返回乱序的新数组
+	 */
+	roa: function roa(arr) //arr为可能出现的元素集合
+	{
+		var temp = new Array() //temp存放生成的随机数组
+		var count = arr.length
+		for (var i = 0; i < count; i++) {
+			var num = Math.floor(Math.random() * arr.length) //生成随机数num
+			temp.push(arr[num]) //获取arr[num]并放入temp
+			arr.splice(num, 1)
+		}
+		return temp
+	},
+	/* 
+	 * 创建动画完成事件
+	 * 通过创建一个div元素，判断他的style属性来确定
+	 * 返回带有当前浏览器前缀的“动画完成事件”
+	 */
+	animationEnd: function animationEnd() {
+		var el = document.createElement('div')
+		// 各种浏览器的动画完成事件
+		var animations = {
+			animation: 'animationend',
+			OAnimation: 'oAnimationEnd',
+			MozAnimation: 'mozAnimationEnd',
+			WebkitAnimation: 'webkitAnimationEnd',
+		}
+		// 枚举事件是否定义
+		for (var t in animations) {
+			if (el.style[t] !== undefined) {
+				// 返回事件
+				return animations[t]
+			}
+		}
+	},
+	/* 
+	 * 向页面插入头像
+	 * addImg(src, x, y, ani)
+	 * 返回值为创建的元素
+	 */
+	addImg: function addImg(src, x, y) {
+		try {
+			if (src != undefined && x != undefined && y != undefined) {
+				var imgEl = $('<img>')
+				imgEl.attr({
+					src: src
+				}).css({
+					left: x * 3 + 'vw',
+					top: y * 3 + 'vw',
+					position: 'absolute',
+					width: '3vw'
+				}).addClass('animated imgAnimation').appendTo('.tx').one(
+					this.animationEnd(),
+					function () {
+						$(this).addClass('animated fast flash')
+					})
+				return imgEl
+			} else {
+				throw new Error('缺少参数')
+			}
+		} catch (error) {
+			console.log('创建图片失败', error, src, x, y)
+		}
+	},
+	showAllItems: function showAllItems(data) {
+		var self = this
+		var items = data.Item
+		// if (items != undefined) {
+		// 	for (var i in items) {
+		// 		console.log(items[i])
+		// 		// addImg(items[i].hear_img, 10, 10)
+		// 		var imgXYarr = roa(createArr(20, 20))
+		// 		addImg(items[i].hear_img, imgXYarr[i % imgXYarr.length].x, imgXYarr[i % imgXYarr.length].y)
+		// 		// addImg('./img/3.jpeg', 10, 10)
+		// 	}
+		// }
+		var timesRun = 0
+		// 产生数组并打乱
+		var imgXYarr = this.roa(this.createArr(20, 20))
+		// 测试图片
+		var img = items
+		// 创建定时器，重复执行timesRun次，延时s
+		var interval = setInterval(function () {
+			console.log("添加", timesRun, "个")
+			if (timesRun === items.length) {
+				// 清除定时器
+				clearInterval(interval)
+				console.log("结束")
+			}
+			var src = img[timesRun].hear_img
+			// 插入图片
+			self.addImg(src,
+				imgXYarr[timesRun % imgXYarr.length].x,
+				imgXYarr[timesRun % imgXYarr.length].y
+			)
+			timesRun += 1
+		}, 1000)
 	}
-
 }
 
 // 创建连接
@@ -111,11 +141,11 @@ function createWebSocket(url) {
 // 初始化连接
 function initEventHandle() {
 	ws.onclose = function () {
-		reconnect(wsUrl)
+		reconnect(wsConf.wsUrl)
 		console.log('连接断开')
 	}
 	ws.onerror = function () {
-		reconnect(wsUrl)
+		reconnect(wsConf.wsUrl)
 		console.log('连接错误')
 	}
 	ws.onopen = function () {
@@ -129,54 +159,37 @@ function initEventHandle() {
 		// 拿到任何消息都说明当前连接是正常的
 		heartCheck.reset().start()
 		// console.log(e.data)
-		var mag = JSON.parse(e.data)
 		// 判断消息类型
-		if (mag.Item) {
-			showAllItems(mag.Item)
+		if (e.data !== wsConf.HeartBeatStr){
+			try {
+				var mag = JSON.parse(e.data)
+				if (typeof mag == 'object' && mag && wsConf.HeartBeatStr) {
+					onMessageCallback()
+				}
+			} catch (e) {
+				console.log('无效的消息')
+			}
+		} else {
+			console.log('收到心跳')
 		}
 	}
 }
 
-function showAllItems (data){
-	var items = data.Item
-	// if (items != undefined) {
-	// 	for (var i in items) {
-	// 		console.log(items[i])
-	// 		// addImg(items[i].hear_img, 10, 10)
-	// 		var imgXYarr = roa(createArr(20, 20))
-	// 		addImg(items[i].hear_img, imgXYarr[i % imgXYarr.length].x, imgXYarr[i % imgXYarr.length].y)
-	// 		// addImg('./img/3.jpeg', 10, 10)
-	// 	}
-	// }
-	var timesRun = 0
-	// 产生数组并打乱
-	var imgXYarr = roa(createArr(20, 20))
-	// 测试图片
-	var img = items
-	// 创建定时器，重复执行timesRun次，延时s
-	var interval = setInterval(function () {
-		console.log("添加", timesRun, "个")
-		if (timesRun === items.length) {
-			// 清除定时器
-			clearInterval(interval)
-			console.log("结束")
-		}
-		var src = img[timesRun].hear_img
-		var ani = 'imgAnimation'
-		// 插入图片
-		addImg(src, imgXYarr[timesRun % imgXYarr.length].x, imgXYarr[timesRun % imgXYarr.length].y, ani)
-		timesRun += 1
-	}, 1000)
+// 接收消息时间回调函数
+function onMessageCallback(data) {
+	if (data.Item) {
+		wall.showAllItems(data.Item)
+	}
 }
 
 // 重连
 function reconnect(url) {
-	if (lockReconnect) return
-	lockReconnect = true
+	if (wsConf.lockReconnect) return
+	wsConf.lockReconnect = true
 	//没连接上会一直重连，设置延迟避免请求过多
 	setTimeout(function () {
 		createWebSocket(url)
-		lockReconnect = false
+		wsConf.lockReconnect = false
 	}, 2000)
 }
 
@@ -195,7 +208,7 @@ var heartCheck = {
 		this.timeoutObj = setTimeout(function () {
 			//这里发送一个心跳，后端收到后，返回一个心跳消息，
 			//onmessage拿到返回的心跳就说明连接正常
-			ws.send("HeartBeat")
+			ws.send(wsConf.HeartBeatStr)
 			self.serverTimeoutObj = setTimeout(function () { //如果超过一定时间还没重置，说明后端主动断开了
 				ws.close() //如果onclose会执行reconnect，我们执行ws.close()就行了.如果直接执行reconnect 会触发onclose导致重连两次
 			}, self.timeout)
@@ -208,7 +221,7 @@ function test(x, y, s) {
 	if (x != undefined && y != undefined && s != undefined) {
 		var timesRun = 0
 		// 产生数组并打乱
-		var imgXYarr = roa(createArr(x, y))
+		var imgXYarr = wall.roa(wall.createArr(x, y))
 		// 测试图片
 		var img = ['./img/1.jpg', './img/2.jpeg', './img/3.jpeg', './img/4.jpg', './img/5.jpg', './img/6.jpg', './img/7.jpg', './img/8.png']
 		// 创建定时器，重复执行timesRun次，延时s
@@ -222,7 +235,7 @@ function test(x, y, s) {
 			var src = img[Math.floor(Math.random() * img.length) % img.length]
 			var ani = 'imgAnimation'
 			// 插入图片
-			addImg(src, imgXYarr[timesRun % imgXYarr.length].x, imgXYarr[timesRun % imgXYarr.length].y, ani)
+			wall.addImg(src, imgXYarr[timesRun % imgXYarr.length].x, imgXYarr[timesRun % imgXYarr.length].y, ani)
 			timesRun += 1
 		}, s)
 	} else {
@@ -235,9 +248,9 @@ function test(x, y, s) {
 
 function start() {
 	// 创建连接
-	createWebSocket(wsUrl)
+	createWebSocket(wsConf.wsUrl)
 	// 获取头像地址
-	// if (lockReconnect)
+	// if (wsConf.lockReconnect)
 	// 	ws.send('getAll')
 	// 创建头像
 
@@ -247,4 +260,4 @@ function start() {
 	// addImg('./img/1.jpg', 1, 1).attr('id', 12);
 }
 
-// start()
+start()
