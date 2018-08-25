9b2b7d346f8c9ac525ae253839870f358f6ad0a0
zkl2333 <i@zkl2333.com>
2018-08-25 22:27:42 +0800

    改正错别字

--- app.js
+++ app.js
@@ -175,10 +175,13 @@ function initEventHandle() {
 	}
 }
 
-// 接收消息时间回调函数
+// 接收消息事件回调函数
 function onMessageCallback(data) {
+	console.log(data)
 	if (data.Item) {
 		wall.showAllItems(data.Item)
+	} else if(data){
+		console.log()
 	}
 }
 
@@ -209,6 +212,7 @@ var heartCheck = {
 			//这里发送一个心跳，后端收到后，返回一个心跳消息，
 			//onmessage拿到返回的心跳就说明连接正常
 			ws.send(wsConf.HeartBeatStr)
+			console.log('发送心跳')
 			self.serverTimeoutObj = setTimeout(function () { //如果超过一定时间还没重置，说明后端主动断开了
 				ws.close() //如果onclose会执行reconnect，我们执行ws.close()就行了.如果直接执行reconnect 会触发onclose导致重连两次
 			}, self.timeout)
